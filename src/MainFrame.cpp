c3ac37059ecbef38649f88b0b3cac94970147d58
M.H.Jim <m.h.jim2357@gmail.com>
2026-04-17 11:36:53 +0600

    A simple mainframe code to test the panel

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -1,6 +1,17 @@
 #include "MainFrame.h"
+#include "RightPanel.h"
 
 MainFrame::MainFrame(const wxString& title)
 : wxFrame(NULL, wxID_ANY, title) {
+    
+    panel = new wxPanel(this);
+    
+    RightPanel *p = new RightPanel(panel);
+    
+    wxBoxSizer *s = new wxBoxSizer(wxVERTICAL);
+    s->Add(p, 1, wxEXPAND);
+    panel->SetSizer(s);
+    
+    
     
 }
